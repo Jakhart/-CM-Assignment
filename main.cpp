e0b5ca73be6db7c429dc0c9cea6232c17175c06d
Jakhart <v.richard@net.estia.fr>
2017-11-23 10:41:01 +0000

    First Working program

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <iostream.h>  //generic IO
+#include <iostream>  //generic IO
 #include <fstream>   //file IO
 #include <stdexcept> //provides exception
 #include "Class/Solver/Explicit/explicit.h"
+
 int main(){
     Explicit prob = Explicit(0.1, 100, 300, 0.001, 0.005);
 
-    std::cout >> prob.getD();
+    std::cout << prob.getD();
 
 }
